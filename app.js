44e830b38923aa5599f9b414d94e171a262b214c
Anima635 <animaakter098@gmail.com>
2025-11-22 19:26:23 +0600

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
 
 const genCompChoice = () => {
     const options = ["rock", "paper", "scissors"];
@@ -11,13 +14,23 @@ const genCompChoice = () => {
 
 const drawGame = () => {
     console.log("Game was draw");
+    msg.innerText = "Game was draw. Play Again";
+    msg.style.backgroundColor = "#081b31";
 }
 
-const showWinnwer = (userWin) => {
+const showWinnwer = (userWin, userchoice, compChoice) => {
     if(userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
         console.log("You win!");
+        msg.innerText = `You win! Your ${userchoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "Green";
     } else {
+        compScore++;
+        compScorePara.innerText = compScore;
         console.log("You lose");
+        msg.innerText = `You Lost. ${compChoice} beats your ${userchoice}`;
+        msg.style.backgroundColor = "red";
     }
 }
 
@@ -38,7 +51,7 @@ const playGame = (userchoice) => {
         } else {
             userWin = compChoice === "rock" ? false : true;
         }
-        showWinnwer(userWin);
+        showWinnwer(userWin, userchoice, compChoice);
     }
 }
 
